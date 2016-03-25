79aefe3005d9dcb02b88e7d576d456554f50dc7e
hyo13 <ho.or13@imperial.ac.uk>
2016-03-25 00:25:55 +0000

    Initial Commit

---
+++ HPC-Q5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HPC-Q5
+//
+//  Created by AO on 25/3/16.
+//  Copyright (c) 2016 hyo13. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
